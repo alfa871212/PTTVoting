c9ad42dde0bfb11a360ec96023ea19fdceed6763
alfa871212 <alfaryzenwsl@DESKTOP-V2H2O5B.localdomain>
2023-02-23 14:30:02 +0800

    integrate url grabbing into statSys class

--- ptt_cchat_user.py
+++ ptt_cchat_user.py
@@ -34,20 +34,70 @@ class Users:
             print(self.id+" has voted for more than 4 comics.")
             
 class StatSys:
-    def __init__(self, userLis, comicLis):
-        self.num_voter = len(userLis)
+    def __init__(self, urlLis):
+        #self.num_voter = len(userLis)
+        self.num_voter = 0
+        self.urlLis = urlLis
         self.result = [0]*120
-        self.vote_once = 0
-        self.vote_twice = 0
-        self.vote_thrice = 0
-        self.vote_four = 0
-        self.vote_times = [0]*4
-        self.comicLis = comicLis
-        self.userLis = userLis
+        self.vote_times = [0]*4 # vote_times[] saves the number of voters who vote once, twice ...
+        #self.comicLis = comicLis # comicLis saves the comic list with sequences(original)
+        #self.userLis = userLis # userList saves the user list with pushing sequence(if repeated, save once)
+        self.comicLis = []
+        self.userLis = []
         self.sortedresList = []
-        self.resDict = dict.fromkeys(comicLis, 0)
-        self.rankDict = dict.fromkeys(comicLis, 0)
+        #self.resDict = dict.fromkeys(comicLis, 0)
+        self.resDict = dict()
+        #self.rankDict = dict.fromkeys(comicLis, 0)
+        self.rankDict = dict()
+    def grabUrl(self):
+        for i in range(len(urlLis)):
+            print("Requesting reponse from URL...")
+            html = requests.get(urlLis[i])
+            print("URL html get!")
+            html_str = html.text
+            soup = BeautifulSoup(html_str, 'lxml')
+            user_id_from_url=soup.find_all(class_="f3 hl push-userid")
+            user_id_lis=[]
+            content_from_url=soup.find_all(class_="f3 push-content")
+            content_lis=[]
+            main_content=soup.find(id='main-content')
+
+            ind = 1
+            for tag in main_content:
+                tmp = str(tag.string)
+                if(tmp[0]=='='):
+                    last_word_index = tmp.find("\n")
+                    self.comicLis.append(tmp[1:last_word_index]+"("+str(ind)+")")
+                    ind = ind+1
+
+            for tag in user_id_from_url:
+                user_id_lis.append(tag.string)
+            for tag in content_from_url:
+                tmp = str(tag.string)
+                tmp = tmp[2:]
+                content_lis.append(tmp)
+
+            for i in range(len(user_id_lis)):
+                push_id = user_id_lis[i]
+                content = content_lis[i]
+                if (check_in_id_lis(self.userLis,push_id)):
+                    print(push_id)
+                    ind = index_in_id_lis(self.userLis,push_id)
+                    self.userLis[ind].rawcontent.append(content)
+                    self.userLis[ind].add_counts()
+                else:    
+                    tmp = []
+                    tmp.append(content)
+                    user = Users(push_id,tmp)
+                    self.userLis.append(user)
+            for i in range(len(self.userLis)):
+                self.userLis[i].create_voted_num()
+                self.userLis[i].check_valid_vote()
+        self.resDict = dict.fromkeys(self.comicLis, 0)
+        self.rankDict = dict.fromkeys(self.comicLis, 0)
+        self.num_voter = len(self.userLis)
     def startVote(self):
+        self.grabUrl()
         for i in range(self.num_voter):
             vote = []
             vote = self.userLis[i].voted_num_lis
@@ -148,95 +198,99 @@ def find_user(lis,id):
             return lis[i]
 
 url = 'https://www.ptt.cc/bbs/C_Chat/M.1676194202.A.7DD.html'
-print("Requesting reponse from URL...")
-html = requests.get(url)
-print("URL html get!")
-html_str = html.text
-soup = BeautifulSoup(html_str, 'lxml')
-user_id_from_url=soup.find_all(class_="f3 hl push-userid")
-user_id_lis=[]
-content_from_url=soup.find_all(class_="f3 push-content")
-content_lis=[]
-main_content=soup.find(id='main-content')
-comic_name=[]
+# print("Requesting reponse from URL...")
+# html = requests.get(url)
+# print("URL html get!")
+# html_str = html.text
+# soup = BeautifulSoup(html_str, 'lxml')
+# user_id_from_url=soup.find_all(class_="f3 hl push-userid")
+# user_id_lis=[]
+# content_from_url=soup.find_all(class_="f3 push-content")
+# content_lis=[]
+# main_content=soup.find(id='main-content')
+# comic_name=[]
 
-ind = 1
-for tag in main_content:
-    tmp = str(tag.string)
-    if(tmp[0]=='='):
-        last_word_index = tmp.find("\n")
-        comic_name.append(tmp[1:last_word_index]+"("+str(ind)+")")
-        ind = ind+1
+# ind = 1
+# for tag in main_content:
+#     tmp = str(tag.string)
+#     if(tmp[0]=='='):
+#         last_word_index = tmp.find("\n")
+#         comic_name.append(tmp[1:last_word_index]+"("+str(ind)+")")
+#         ind = ind+1
 
-for tag in user_id_from_url:
+# for tag in user_id_from_url:
 
-    user_id_lis.append(tag.string)
-for tag in content_from_url:
-    tmp = str(tag.string)
-    tmp = tmp[2:]
-    content_lis.append(tmp)
+#     user_id_lis.append(tag.string)
+# for tag in content_from_url:
+#     tmp = str(tag.string)
+#     tmp = tmp[2:]
+#     content_lis.append(tmp)
 
-user_class_lis=[]
-for i in range(len(user_id_lis)):
-    push_id = user_id_lis[i]
-    content = content_lis[i]
-    if (check_in_id_lis(user_class_lis,push_id)):
-        print(push_id)
-        ind = index_in_id_lis(user_class_lis,push_id)
-        user_class_lis[ind].rawcontent.append(content)
-        user_class_lis[ind].add_counts()
-    else:    
-        tmp = []
-        tmp.append(content)
-        user = Users(push_id,tmp)
-        user_class_lis.append(user)
+# user_class_lis=[]
+# for i in range(len(user_id_lis)):
+#     push_id = user_id_lis[i]
+#     content = content_lis[i]
+#     if (check_in_id_lis(user_class_lis,push_id)):
+#         print(push_id)
+#         ind = index_in_id_lis(user_class_lis,push_id)
+#         user_class_lis[ind].rawcontent.append(content)
+#         user_class_lis[ind].add_counts()
+#     else:    
+#         tmp = []
+#         tmp.append(content)
+#         user = Users(push_id,tmp)
+#         user_class_lis.append(user)
 
-for i in range(len(user_class_lis)):
-    user_class_lis[i].create_voted_num()
-    user_class_lis[i].check_valid_vote()
+# for i in range(len(user_class_lis)):
+#     user_class_lis[i].create_voted_num()
+#     user_class_lis[i].check_valid_vote()
 
 
 url2 = 'https://www.ptt.cc/bbs/C_Chat/M.1676455607.A.9CA.html'
-print("Requesting reponse from URL...")
-html2 = requests.get(url2)
-print("URL html get!")
-html_str2 = html2.text
-soup2 = BeautifulSoup(html_str2, 'lxml')
-user_id_from_url2=soup2.find_all(class_="f3 hl push-userid")
-user_id_lis2=[]
-content_from_url2=soup2.find_all(class_="f3 push-content")
-content_lis2=[]
-main_content2=soup2.find(id='main-content')
-comic_name2=[]
+urlLis = []
+urlLis.append(url)
+urlLis.append(url2)
+# print("Requesting reponse from URL...")
+# html2 = requests.get(url2)
+# print("URL html get!")
+# html_str2 = html2.text
+# soup2 = BeautifulSoup(html_str2, 'lxml')
+# user_id_from_url2=soup2.find_all(class_="f3 hl push-userid")
+# user_id_lis2=[]
+# content_from_url2=soup2.find_all(class_="f3 push-content")
+# content_lis2=[]
+# main_content2=soup2.find(id='main-content')
+# comic_name2=[]
 
-ind = 1
+# ind = 1
 
-for tag in user_id_from_url2:
-    user_id_lis2.append(tag.string)
-for tag in content_from_url2:
-    tmp = str(tag.string)
-    tmp = tmp[2:]
-    content_lis2.append(tmp)
+# for tag in user_id_from_url2:
+#     user_id_lis2.append(tag.string)
+# for tag in content_from_url2:
+#     tmp = str(tag.string)
+#     tmp = tmp[2:]
+#     content_lis2.append(tmp)
 
-for i in range(len(user_id_lis2)):
-    push_id = user_id_lis2[i]
-    content = content_lis2[i]
-    if (check_in_id_lis(user_class_lis,push_id)):
-        print(push_id)
-        ind = index_in_id_lis(user_class_lis,push_id)
-        user_class_lis[ind].rawcontent.append(content)
-        user_class_lis[ind].add_counts()
-    else:    
-        tmp = []
-        tmp.append(content)
-        user = Users(push_id,tmp)
-        user_class_lis.append(user)
+# for i in range(len(user_id_lis2)):
+#     push_id = user_id_lis2[i]
+#     content = content_lis2[i]
+#     if (check_in_id_lis(user_class_lis,push_id)):
+#         print(push_id)
+#         ind = index_in_id_lis(user_class_lis,push_id)
+#         user_class_lis[ind].rawcontent.append(content)
+#         user_class_lis[ind].add_counts()
+#     else:    
+#         tmp = []
+#         tmp.append(content)
+#         user = Users(push_id,tmp)
+#         user_class_lis.append(user)
 
-for i in range(len(user_class_lis)):
-    user_class_lis[i].create_voted_num()
-    user_class_lis[i].check_valid_vote()
+# for i in range(len(user_class_lis)):
+#     user_class_lis[i].create_voted_num()
+#     user_class_lis[i].check_valid_vote()
 
 
-statsys = StatSys(user_class_lis,comic_name)
+#statsys = StatSys(user_class_lis,comic_name,urlLis)
+statsys = StatSys(urlLis)
 statsys.startVote()
 statsys.showResult()
