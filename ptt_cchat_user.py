9a74213f939d234db89b2f249a2237db40aa5c06
alfaMac <alfamac@alfaMac.local>
2023-02-26 20:52:29 +0800

    add some minor fixes

--- ptt_cchat_user.py
+++ ptt_cchat_user.py
@@ -2,111 +2,144 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
 class Users:
     def __init__(self, id, rawcontent):
-        self.id = id 
+        self.id = id
         # raw content is a lis(maybe with more than two comments)
         self.rawcontent = rawcontent
-        self.voted_num_lis =[]
+        self.voted_num_lis = []
         self.num_voted = 0
         self.counts = 1
+
     def push_counts(self):
         return self.counts
+
     def add_counts(self):
-        self.counts = self.counts+1
+        self.counts = self.counts + 1
+
     def create_voted_num(self):
-        temp=[]
-        delimiter = [',','、','.','。','@','，']
+        temp = []
+        delimiter = [",", "、", ".", "。", "@", "，"]
         for i in range(self.counts):
             ind = self.counts - i - 1
-            #temp = self.rawcontent[ind].split()
+            # temp = self.rawcontent[ind].split()
             temp = self.rawcontent[ind].split()
             for k in delimiter:
-                #temp2 = self.rawcontent[ind].split(k)
-                temp2 = self.rawcontent[ind].replace(k,' ').split()
-                if(len(temp2)>=len(temp)):
-                    #print(self.rawcontent[ind])
+                # temp2 = self.rawcontent[ind].split(k)
+                temp2 = self.rawcontent[ind].replace(k, " ").split()
+                if len(temp2) >= len(temp):
+                    # print(self.rawcontent[ind])
                     temp = temp2
 
             for j in range(len(temp)):
-                if (temp[j].isdigit()):
-                    if(int(temp[j]) not in self.voted_num_lis):
-                        if (int(temp[j])<=120):
-                            if (len(self.voted_num_lis)<4):
+                if temp[j].isdigit():
+                    if int(temp[j]) not in self.voted_num_lis:
+                        if int(temp[j]) <= 120:
+                            if len(self.voted_num_lis) < 4:
                                 self.voted_num_lis.append(int(temp[j]))
         list(set(self.voted_num_lis))
 
     def check_valid_vote(self):
-        if (len(self.voted_num_lis)>=5):
-            print(self.id+" has voted for more than 4 comics.")
-            
+        if len(self.voted_num_lis) >= 5:
+            print(self.id + " has voted for more than 4 comics.")
+
+
+class Comic:
+    def __init__(self, comicName, index):
+        self.comicName = comicName
+        self.index = index
+        self.votedNum = 0
+        self.rank = 0
+
+    def beVoted(self):
+        self.votedNum = self.votedNum + 1
+
+    def returnInfo(self, input):
+        if isinstance(input, int):
+            return self.comicName
+        else:
+            return self.index
+
+
 class StatSys:
     def __init__(self, urlLis):
-        #self.num_voter = len(userLis)
+        # self.num_voter = len(userLis)
         self.num_voter = 0
-        self.repeated_voter=[]
+        self.repeated_voter = []
         self.urlLis = urlLis
-        self.result = [0]*120
-        self.vote_times = [0]*4 # vote_times[] saves the number of voters who vote once, twice ...
-        #self.comicLis = comicLis # comicLis saves the comic list with sequences(original)
-        #self.userLis = userLis # userList saves the user list with pushing sequence(if repeated, save once)
+        self.result = [0] * 120
+        self.vote_times = [
+            0
+        ] * 4  # vote_times[] saves the number of voters who vote once, twice ...
+        # self.comicLis = comicLis # comicLis saves the comic list with sequences(original)
+        # self.userLis = userLis # userList saves the user list with pushing sequence(if repeated, save once)
         self.comicLis = []
         self.userLis = []
         self.sortedresList = []
-        #self.resDict = dict.fromkeys(comicLis, 0)
+        # self.resDict = dict.fromkeys(comicLis, 0)
         self.resDict = dict()
-        #self.rankDict = dict.fromkeys(comicLis, 0)
+        # self.rankDict = dict.fromkeys(comicLis, 0)
         self.rankDict = dict()
-        self.votedUserDict = dict() #create a dict whose key is comicName, value is a list contained the users who voted
-    def index_in_id_lis(self,lis,id):
-        temp=[]
+        self.votedUserDict = (
+            dict()
+        )  # create a dict whose key is comicName, value is a list contained the users who voted
+        self.newComicLis = []
+
+    def index_in_id_lis(self, lis, id):
+        temp = []
         for i in range(len(lis)):
             temp.append(lis[i].id)
         if id in temp:
             return temp.index(id)
         else:
             return
-    def check_in_id_lis(self,lis,id):
-        temp=[]
+
+    def check_in_id_lis(self, lis, id):
+        temp = []
         for i in range(len(lis)):
             temp.append(lis[i].id)
         return id in temp
-    def find_user_new(self,id):
+
+    def find_user_new(self, id):
         for i in range(self.num_voter):
             if self.userLis[i].id == id:
                 print(self.userLis[i].voted_num_lis)
-    def find_all_users_vote_for_this(self,num):
-        temp=[]
+
+    def find_all_users_vote_for_this(self, num):
+        temp = []
         for i in range(self.num_voter):
             if num in self.userLis[i].voted_num_lis:
-                #print(self.userLis[i].id)
+                # print(self.userLis[i].id)
                 temp.append(self.userLis[i].id)
-        self.votedUserDict[self.comicLis[num-1]] = temp
-    def find_user(self,lis,id):
+        self.votedUserDict[self.comicLis[num - 1]] = temp
+
+    def find_user(self, lis, id):
         for i in range(len(lis)):
             if lis[i].id == id:
                 return lis[i]
+
     def grabUrl(self):
         for i in range(len(urlLis)):
             print("Requesting reponse from URL...")
             html = requests.get(urlLis[i])
             print("URL html get!")
             html_str = html.text
-            soup = BeautifulSoup(html_str, 'lxml')
-            user_id_from_url=soup.find_all(class_="f3 hl push-userid")
-            user_id_lis=[]
-            content_from_url=soup.find_all(class_="f3 push-content")
-            content_lis=[]
-            main_content=soup.find(id='main-content')
+            soup = BeautifulSoup(html_str, "lxml")
+            user_id_from_url = soup.find_all(class_="f3 hl push-userid")
+            user_id_lis = []
+            content_from_url = soup.find_all(class_="f3 push-content")
+            content_lis = []
+            main_content = soup.find(id="main-content")
 
             ind = 1
             for tag in main_content:
                 tmp = str(tag.string)
-                if(tmp[0]=='='):
+                if tmp[0] == "=":
                     last_word_index = tmp.find("\n")
-                    self.comicLis.append(tmp[1:last_word_index]+"("+str(ind)+")")
-                    ind = ind+1
+                    self.comicLis.append(tmp[1:last_word_index] + "(" + str(ind) + ")")
+                    tmpComic = Comic(comicName=tmp[1:last_word_index], index=ind)
+                    self.newComicLis.append(tmpComic)
+                    ind = ind + 1
 
             for tag in user_id_from_url:
                 user_id_lis.append(tag.string)
@@ -118,16 +151,16 @@ class StatSys:
             for i in range(len(user_id_lis)):
                 push_id = user_id_lis[i]
                 content = content_lis[i]
-                if (self.check_in_id_lis(self.userLis,push_id)):
-                    #print(push_id)
+                if self.check_in_id_lis(self.userLis, push_id):
+                    # print(push_id)
                     self.repeated_voter.append(push_id)
-                    ind = self.index_in_id_lis(self.userLis,push_id)
+                    ind = self.index_in_id_lis(self.userLis, push_id)
                     self.userLis[ind].rawcontent.append(content)
                     self.userLis[ind].add_counts()
-                else:    
+                else:
                     tmp = []
                     tmp.append(content)
-                    user = Users(push_id,tmp)
+                    user = Users(push_id, tmp)
                     self.userLis.append(user)
             for i in range(len(self.userLis)):
                 self.userLis[i].create_voted_num()
@@ -135,98 +168,126 @@ class StatSys:
         self.resDict = dict.fromkeys(self.comicLis, 0)
         self.rankDict = dict.fromkeys(self.comicLis, 0)
         self.num_voter = len(self.userLis)
+
     def startVote(self):
         self.grabUrl()
         for i in range(self.num_voter):
             vote = []
             vote = self.userLis[i].voted_num_lis
-            self.vote_times[len(vote)-1] = self.vote_times[len(vote)-1]+1
+            self.vote_times[len(vote) - 1] = (
+                self.vote_times[len(vote) - 1] + 1
+            )  # update vote_times list with determine the length of voted_num_lis
             for j in range(len(vote)):
                 vote_ind = vote[j]
-                #print(vote_ind)
-                self.resDict[self.comicLis[vote_ind-1]]=self.resDict[self.comicLis[vote_ind-1]]+1
-                #self.result[vote_ind-1] = self.result[vote_ind-1]+1
-        
-        sorted_all_results = sorted(self.resDict.items(), key=lambda x:x[1],reverse=True)
-        #print(sorted_all_results)
+                # print(vote_ind)
+                self.resDict[self.comicLis[vote_ind - 1]] = (
+                    self.resDict[self.comicLis[vote_ind - 1]] + 1
+                )
+                # self.result[vote_ind-1] = self.result[vote_ind-1]+1
+
+        sorted_all_results = sorted(
+            self.resDict.items(), key=lambda x: x[1], reverse=True
+        )
+        # print(sorted_all_results)
         rank = 1
         repeated_times = 1
         buffer = -1
         for i in range(len(sorted_all_results)):
-            rank = i+1
-            if (sorted_all_results[i][1] != buffer):
+            rank = i + 1
+            if sorted_all_results[i][1] != buffer:
                 comic_name = sorted_all_results[i][0]
                 self.rankDict[comic_name] = rank
-                repeated_times = 1 
+                repeated_times = 1
             else:
                 comic_name = sorted_all_results[i][0]
                 self.rankDict[comic_name] = rank - repeated_times
-                repeated_times = repeated_times+1
+                repeated_times = repeated_times + 1
             buffer = sorted_all_results[i][1]
         self.sortedresList = sorted_all_results
         for i in range(len(self.comicLis)):
-            self.find_all_users_vote_for_this(i+1)
-    def checkRelation(self, num1,num2): # return P(vote num1 and num2| vote num1)
+            self.find_all_users_vote_for_this(i + 1)
+
+    def checkRelation(self, num1, num2):  # return P(vote num1 and num2| vote num1)
         vote_for_num1 = 0
-        num1_comic_name = self.comicLis[num1-1]
+        num1_comic_name = self.comicLis[num1 - 1]
         vote_for_num1_num2 = 0
-        num2_comic_name = self.comicLis[num2-1]
+        num2_comic_name = self.comicLis[num2 - 1]
 
         for i in range(self.num_voter):
-            if (num1 in self.userLis[i].voted_num_lis):
+            if num1 in self.userLis[i].voted_num_lis:
                 vote_for_num1 = vote_for_num1 + 1
-                if (num2 in self.userLis[i].voted_num_lis):
+                if num2 in self.userLis[i].voted_num_lis:
                     vote_for_num1_num2 = vote_for_num1_num2 + 1
 
-        out = "Percentage of voting for "+num1_comic_name +" who also vote for "+num2_comic_name +" is "+ str(vote_for_num1_num2/vote_for_num1)
+        out = (
+            "Percentage of voting for "
+            + num1_comic_name
+            + " who also vote for "
+            + num2_comic_name
+            + " is "
+            + str(vote_for_num1_num2 / vote_for_num1)
+        )
         print(out)
-    
-    def isValidSearchNum(self,num):
-        return num > 0 and num <=120
-    def checkRank(self,search):
-        if (isinstance(search, int)):
-            if(self.isValidSearchNum(search)):
-                comic = self.comicLis[search-1]
+
+    def isValidSearchNum(self, num):
+        return num > 0 and num <= 120
+
+    def checkRank(self, search):
+        if isinstance(search, int):
+            if self.isValidSearchNum(search):
+                comic = self.comicLis[search - 1]
                 print(self.rankDict[comic])
             else:
                 print("Invalid search num!!")
         else:
             print(self.rankDict[search])
-    def isValidSearchNum(self,num):
-        return num > 0 and num <=120
-    def showResult(self):
-        #print(self.resDict)
 
-        sorted_all_results = sorted(self.resDict.items(), key=lambda x:x[1],reverse=True)
-        #print(sorted_all_results)
+    def isValidSearchNum(self, num):
+        return num > 0 and num <= 120
+
+    def showResult(self):
+        # print(self.resDict)
+
+        sorted_all_results = sorted(
+            self.resDict.items(), key=lambda x: x[1], reverse=True
+        )
+        # print(sorted_all_results)
         rank = 1
         repeated_times = 1
         buffer = -1
         for i in range(len(self.sortedresList)):
-            rank = i+1
-            if (self.sortedresList[i][1] != buffer):
-                output=str(rank)+". "+self.sortedresList[i][0]+" "+str(self.sortedresList[i][1])
+            rank = i + 1
+            if self.sortedresList[i][1] != buffer:
+                output = (
+                    str(rank)
+                    + ". "
+                    + self.sortedresList[i][0]
+                    + " "
+                    + str(self.sortedresList[i][1])
+                )
                 print(output)
-                repeated_times = 1 
+                repeated_times = 1
             else:
-                output=str(rank-repeated_times)+". "+self.sortedresList[i][0]+" "+str(self.sortedresList[i][1])
-                repeated_times = repeated_times+1
+                output = (
+                    str(rank - repeated_times)
+                    + ". "
+                    + self.sortedresList[i][0]
+                    + " "
+                    + str(self.sortedresList[i][1])
+                )
+                repeated_times = repeated_times + 1
 
                 print(output)
             buffer = self.sortedresList[i][1]
 
 
-
-
-
-
-url = 'https://www.ptt.cc/bbs/C_Chat/M.1676194202.A.7DD.html'
-url2 = 'https://www.ptt.cc/bbs/C_Chat/M.1676455607.A.9CA.html'
+url = "https://www.ptt.cc/bbs/C_Chat/M.1676194202.A.7DD.html"
+url2 = "https://www.ptt.cc/bbs/C_Chat/M.1676455607.A.9CA.html"
 urlLis = []
 urlLis.append(url)
 urlLis.append(url2)
 
-#statsys = StatSys(user_class_lis,comic_name,urlLis)
+# statsys = StatSys(user_class_lis,comic_name,urlLis)
 statsys = StatSys(urlLis)
 statsys.startVote()
 statsys.showResult()
